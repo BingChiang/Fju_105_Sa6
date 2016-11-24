8a5e5c8b6cbb25067afc61b4707504edf4ccbf0a
user <user@Cheyu>
2016-11-24 14:12:12 +0800

    連資料庫的

---
+++ fju_sa6/src/main/java/fju/im/sa6/webapp/dao/impl/ManagerDAOImpl.java
@@ -0,0 +1,33 @@
+package fju.im.sa6.webapp.dao.impl;
+
+import fju.im.sa6.entity.Manager;
+import fju.im.sa6.webapp.dao.ManagerDAO;
+import java.sql.Connection;
+
+public class ManagerDAOImpl implements ManagerDAO {
+
+	@Override
+	public void addStaff(Manager addstaff) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void set(Manager setstaff) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void remove(Manager removestaff) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void inquireAllWorktime(Manager inquireAWKT) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ fju_sa6/src/main/java/fju/im/sa6/webapp/dao/impl/OrderDefaultDAOImpl.java
@@ -0,0 +1,33 @@
+package fju.im.sa6.webapp.dao.impl;
+
+import fju.im.sa6.entity.OrderDefault;
+import fju.im.sa6.webapp.dao.OrderDefaultDAO;
+import java.sql.Connection;
+
+public class OrderDefaultDAOImpl implements OrderDefaultDAO {
+
+	@Override
+	public void add(OrderDefault addOrder) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void set(OrderDefault setOrder) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void remove(OrderDefault removeOrder) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public OrderDefault get(OrderDefault getOrder) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
