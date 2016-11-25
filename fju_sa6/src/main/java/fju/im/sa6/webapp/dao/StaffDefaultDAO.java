69dea0fefb76e8f1954b1ba4195482af237b81ce
allen <allen@DESKTOP-I9I0AVJ>
2016-11-25 13:38:15 +0800

    yes

--- fju_sa6/src/main/java/fju/im/sa6/webapp/dao/StaffDefaultDAO.java
+++ fju_sa6/src/main/java/fju/im/sa6/webapp/dao/StaffDefaultDAO.java
@@ -3,5 +3,5 @@ package fju.im.sa6.webapp.dao;
 import fju.im.sa6.entity.StaffDefault;
 
 public interface StaffDefaultDAO {
-	public StaffDefault get(StaffDefault getStaff);
+	public StaffDefault get(int searchNum);
 }
