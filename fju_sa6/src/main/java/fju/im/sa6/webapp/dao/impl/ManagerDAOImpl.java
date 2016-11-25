9b35992fa3bc421811ddb9f7ed0f1607e6f8cb4b
allen <allen@DESKTOP-I9I0AVJ>
2016-11-25 13:38:15 +0800

    yes

--- fju_sa6/src/main/java/fju/im/sa6/webapp/dao/impl/ManagerDAOImpl.java
+++ fju_sa6/src/main/java/fju/im/sa6/webapp/dao/impl/ManagerDAOImpl.java
@@ -3,25 +3,93 @@ package fju.im.sa6.webapp.dao.impl;
 import fju.im.sa6.entity.Manager;
 import fju.im.sa6.webapp.dao.ManagerDAO;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
 
 public class ManagerDAOImpl implements ManagerDAO {
+	private DataSource dataSource;
+	private Connection conn = null ;
+	private ResultSet rs = null ;
+	private PreparedStatement smt = null ;
 
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 	@Override
 	public void addStaff(Manager addstaff) {
 		// TODO Auto-generated method stub
-		
+		String sql = "INSERT INTO manager (staff_name, staff_lv)VALUE(?, ?)";
+		try {
+			conn = dataSource.getConnection();
+			smt = conn.prepareStatement(sql);
+
+			smt.setString(1,addstaff.getStaffName());
+			smt.setString(1,addstaff.getStaffLv());
+			smt.executeUpdate();			
+			smt.close();
+ 
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+ 
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {}
+			}
+		}
 	}
 
 	@Override
 	public void set(Manager setstaff) {
 		// TODO Auto-generated method stub
+		String sql = "UPDATE manager (staff_name,staff_Lv) VALUES(?, ?)";	
+		try {
+			conn = dataSource.getConnection();
+			smt = conn.prepareStatement(sql);
+			smt.setString(1,setstaff.getStaffName());
+			smt.setInt(2,setstaff.getStaffLv());
+			smt.executeUpdate();			
+			smt.close();
+ 
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+ 
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {}
+			}
+		}
+
 
 	}
 
 	@Override
 	public void remove(Manager removestaff) {
 		// TODO Auto-generated method stub
-
+		String sql = "DELETE FROM product WHERE staff_num = ?";
+		try {
+			conn = dataSource.getConnection();
+			smt = conn.prepareStatement(sql);
+			smt.setInt(1,removestaff.getStaffNum());
+			smt.executeUpdate();			
+			smt.close();
+ 
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+ 
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {}
+			}
+		}
 	}
 
 	@Override
@@ -33,6 +101,7 @@ public class ManagerDAOImpl implements ManagerDAO {
 	@Override
 	public void setlevel(int staffNum) {
 		// TODO Auto-generated method stub
+		
 		
 	}
 
