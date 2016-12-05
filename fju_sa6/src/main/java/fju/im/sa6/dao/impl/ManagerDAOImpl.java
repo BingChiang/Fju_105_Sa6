a127ac4f8a78e92aa6b381ef9378366487673fca
user <user@Cheyu>
2016-12-05 15:37:27 +0800

    小改動

--- fju_sa6/src/main/java/fju/im/sa6/dao/impl/ManagerDAOImpl.java
+++ fju_sa6/src/main/java/fju/im/sa6/dao/impl/ManagerDAOImpl.java
@@ -138,7 +138,7 @@ public class ManagerDAOImpl implements ManagerDAO {
 				if (staffDefault.getStaffLevel() == 0) {
 					smt.setInt(1, staffDefault.getStaffLevel());
 				} else {
-					smt.setInt(0, staffDefault.getStaffLevel());
+					smt.setInt(1, staffDefault.getStaffLevel());
 				}
 			} catch (SQLException e) {
 				throw new RuntimeException(e);
